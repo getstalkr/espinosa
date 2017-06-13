78281d6ccba7e29e722c6b3550481adc63aea6f2
Matheus Brasil <matheus.brasil10@gmail.com>
2017-06-13 18:14:30 -0300

    feat(core): add latency for pinging tests demonstration

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const micro = require('micro');
-const { router, get } = require('microrouter');
-const random = require('./helpers/random');
+const {router, get} = require('microrouter');
+const {delay} = require('./helpers/delay');
+const {element, number} = require('./helpers/random');
 const products = require('./products');
 
-const randomHandler = async (req, res) =>
-  micro.send(res, 200, await Promise.resolve(random(products)));
+const randomHandler = async function (req, res) {
+  const timeout = number(5000, 15000);
+  await delay(timeout);
+  micro.send(res, 200, await Promise.resolve(element(products)));
+};
 
-const allHandler = async (req, res) =>
+const allHandler = async function (req, res) {
+  const timeout = number(5000, 15000);
+  await delay(timeout);
   micro.send(res, 200, await Promise.resolve(products));
+};
 
 const server = micro(router(
   get('/random', randomHandler),
