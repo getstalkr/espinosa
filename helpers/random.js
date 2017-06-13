8ff4eee574f3914ff92d021dfa1b29dd59650435
Matheus Brasil <matheus.brasil10@gmail.com>
2017-06-13 18:14:05 -0300

    feat(random helper): add another helper to generate random number from interval

--- helpers/random.js
+++ helpers/random.js
@@ -1,7 +1,14 @@
 const uniqueRandomArray = require('unique-random-array');
 
-function random(arr) {
+const element = function (arr) {
   return uniqueRandomArray(arr)();
-}
+};
 
-module.exports = random;
+const number = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+exports.element = element;
+exports.number = number;
