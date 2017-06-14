f80e1bfe8bb7203db404bcfb95c979ad2fcdbd93
Matheus Brasil <matheus.brasil10@gmail.com>
2017-06-14 03:13:45 -0300

    feat(core): kill process for uptime demonstration

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const micro = require('micro');
 const {router, get} = require('microrouter');
 const {delay} = require('./helpers/delay');
-const {element, number} = require('./helpers/random');
+const {element, number, boolean} = require('./helpers/random');
 const products = require('./products');
 
 const randomHandler = async function (req, res) {
+
+  if (boolean()) {
+    process.exit(1);
+  }
+
   const timeout = number(5000, 15000);
   await delay(timeout);
   micro.send(res, 200, await Promise.resolve(element(products)));
 };
 
 const allHandler = async function (req, res) {
+
+  if (boolean()) {
+    process.exit(1);
+  }
+
   const timeout = number(5000, 15000);
   await delay(timeout);
   micro.send(res, 200, await Promise.resolve(products));
